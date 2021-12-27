319f80392fc36ce3224e81dce015c5259b0559f3
hogehoge <hogehoge@gmail.com>
2021-12-27 11:18:40 +0900

    c2_16追加 dictionaryの削除

---
+++ day3/c2_16.py
@@ -0,0 +1,8 @@
+scores = {'network':60, 
+          'database':80, 
+          'security':50
+          };
+#scores = {'network':60, 'database':80, 'security':50}
+scores['programming']=65#要素の追加
+del scores['security']
+print(scores)
