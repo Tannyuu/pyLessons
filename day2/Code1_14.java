939a09c4e93a9030cd6dda87c9ec1bd6e6bafcae
hogehoge <hogehoge@gmail.com>
2021-12-27 10:19:06 +0900

    first init

---
+++ day2/Code1_14.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Code1_14 {
+    public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+        System.out.println("あなたの名前を入力してください>>");
+	String name=sc.nextLine();
+	System.out.printf("おお！%sが来るのを待っておったぞ！",name);
+    }
+}
---
+++ day2/dog.py
@@ -0,0 +1,4 @@
+name=str(input('あなたの犬の名前>>'))
+age=int(input('あなたの犬の年齢>>'))
+ageA=age*7
+print(f'{name}{age}歳は人間換算で{ageA}歳です')
