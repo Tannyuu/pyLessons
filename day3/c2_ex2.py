aef59b945e1063cc6dee0f2b6112d2b840b6e0ff
hogehoge <hogehoge@gmail.com>
2021-12-28 12:33:01 +0900

    c2_ex2 c2_ex3 修正

--- day3/c2_ex2.py
+++ day3/c2_ex2.py
@@ -5,11 +5,14 @@ li.append(int(input('理科>>')))
 li.append(int(input('社会>>')))
 li.append(int(input('英語>>')))
 
-'''
+"""
 let li=[];
-li.push(20)
-li.push(10)
+li.push(10);
+li.push(20);
 let sum=0;
-'''
-#console.log(`合計点${s},平均点${avg}`)#jsの書きかた
+for(let i=0;i<li.length;i++){
+    sum+=li[i];
+}
+console.log(`合計点${sum},平均点${sum/li.length}`)
+"""
 print(f'合計点{sum(li)},平均点{sum(li)/len(li)}')
