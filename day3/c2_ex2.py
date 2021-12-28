48744874801d981b9a84e22cbd627fbceb294d10
hogehoge <hogehoge@gmail.com>
2021-12-28 11:43:13 +0900

    c2_ex2 c2_ex3追加

---
+++ day3/c2_ex2.py
@@ -0,0 +1,15 @@
+li=[]
+li.append(int(input('国語>>')))
+li.append(int(input('算数>>')))
+li.append(int(input('理科>>')))
+li.append(int(input('社会>>')))
+li.append(int(input('英語>>')))
+
+'''
+let li=[];
+li.push(20)
+li.push(10)
+let sum=0;
+'''
+#console.log(`合計点${s},平均点${avg}`)#jsの書きかた
+print(f'合計点{sum(li)},平均点{sum(li)/len(li)}')
---
+++ day3/c2_ex3.py
@@ -0,0 +1,4 @@
+hb1={}
+hb2={}
+input()
+print(f'{len(hb1 & hb2)/len(hb1 & hb2)*100}')
